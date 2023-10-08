71312154784fa2b9494649fb255c3c20297a7e5c
swapan-miah <swaponmiah120@gmail.com>
2023-10-08 18:52:16 +0600

    click every navlink auto hide menu

--- src/Component/Shared/Nav/Nav.jsx
+++ src/Component/Shared/Nav/Nav.jsx
@@ -10,12 +10,8 @@ import { Link, NavLink } from "react-router-dom";
 function Nav() {
   function myFunction() {
     console.log("click myFunction");
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+    var list = document.getElementById("myTopnav").classList;
+    list.toggle("responsive");
   }
   return (
     <>
@@ -23,24 +19,50 @@ function Nav() {
         <div className="navpadding rounded-4 ">
           <div className="topnav rounded-4 p-2" id="myTopnav">
             <NavLink to="/">
-              <FaHouseChimney className=" text-white  me-2 "></FaHouseChimney>
+              <FaHouseChimney className=" text-white  me-2 pb-1 pb-md-0 "></FaHouseChimney>
               হোম
             </NavLink>
-            <NavLink to="/school-intro"> স্কুল পরিচিতি </NavLink>
-            <NavLink to="/teachers-worker">শিক্ষক-কর্মচারীর তথ্য </NavLink>
-            <NavLink to="/student-info">শিক্ষার্থীর তথ্য </NavLink>
-            <NavLink to="/syllabus">সিলেবাস</NavLink>
-            <NavLink to="/routine">রুটিন</NavLink>
-            <NavLink to="/notice">নোটিশ</NavLink>
-            <NavLink to="/result">রেজাল্ট</NavLink>
-            <NavLink to="/Committee">কমিটি</NavLink>
-            <NavLink to="/ssc-board-result">
+            <NavLink to="/school-intro" onClick={myFunction}>
+              {" "}
+              স্কুল পরিচিতি
+            </NavLink>
+            <NavLink to="/teachers-worker" onClick={myFunction}>
+              শিক্ষক-কর্মচারীর তথ্য{" "}
+            </NavLink>
+            <NavLink to="/student-info" onClick={myFunction}>
+              শিক্ষার্থীর তথ্য{" "}
+            </NavLink>
+            <NavLink to="/syllabus" onClick={myFunction}>
+              সিলেবাস
+            </NavLink>
+            <NavLink to="/routine" onClick={myFunction}>
+              রুটিন
+            </NavLink>
+            <NavLink to="/notice" onClick={myFunction}>
+              নোটিশ
+            </NavLink>
+            <NavLink to="/result" onClick={myFunction}>
+              রেজাল্ট
+            </NavLink>
+            <NavLink to="/Committee" onClick={myFunction}>
+              কমিটি
+            </NavLink>
+            <NavLink to="/ssc-board-result" onClick={myFunction}>
               বিগত এস এস সি বোর্ড পরীক্ষার ফল
             </NavLink>
-            <NavLink to="/scholarship">বৃত্তি , উপ-বৃত্তি </NavLink>
-            <NavLink to="/meritorious-student">কৃতি শিক্ষার্থী </NavLink>
-            <NavLink to="/approval"> অনুমোদন ও স্বীকৃতি</NavLink>
-            <NavLink to="/mpo">এমপিও ও জাতীয়করনের তথ্য</NavLink>
+            <NavLink to="/scholarship" onClick={myFunction}>
+              বৃত্তি , উপ-বৃত্তি{" "}
+            </NavLink>
+            <NavLink to="/meritorious-student" onClick={myFunction}>
+              কৃতি শিক্ষার্থী{" "}
+            </NavLink>
+            <NavLink to="/approval" onClick={myFunction}>
+              {" "}
+              অনুমোদন ও স্বীকৃতি
+            </NavLink>
+            <NavLink to="/mpo" onClick={myFunction}>
+              এমপিও ও জাতীয়করনের তথ্য
+            </NavLink>
             <div className="dropdown1">
               <button className="dropbtn">
                 গ্যালারি
@@ -50,8 +72,12 @@ function Nav() {
                 ></BsCaretDownFill>
               </button>
               <div className="dropdown-content">
-                <NavLink to="/img-gallary">ছবি </NavLink>
-                <NavLink to="/video-gallary">ভিডিও </NavLink>
+                <NavLink to="/img-gallary" onClick={myFunction}>
+                  ছবি{" "}
+                </NavLink>
+                <NavLink to="/video-gallary" onClick={myFunction}>
+                  ভিডিও{" "}
+                </NavLink>
               </div>
             </div>
             <div className="dropdown1">
@@ -63,9 +89,15 @@ function Nav() {
                 ></BsCaretDownFill>
               </button>
               <div className="dropdown-content">
-                <NavLink to="/subarna-jayanthi-speak">বার্তা </NavLink>
-                <NavLink to="/subarna-jayanthi-img">ছবি </NavLink>
-                <NavLink to="/subarna-jayanthi-video">ভিডিও</NavLink>
+                <NavLink to="/subarna-jayanthi-speak" onClick={myFunction}>
+                  বার্তা{" "}
+                </NavLink>
+                <NavLink to="/subarna-jayanthi-img" onClick={myFunction}>
+                  ছবি{" "}
+                </NavLink>
+                <NavLink to="/subarna-jayanthi-video" onClick={myFunction}>
+                  ভিডিও
+                </NavLink>
               </div>
             </div>
 
