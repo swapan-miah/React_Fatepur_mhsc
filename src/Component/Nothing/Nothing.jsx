ed31e6df0b31a783bfeb569edafe438b5e342766
swapan-miah <swaponmiah120@gmail.com>
2023-10-09 07:32:08 +0600

    our website ready for production

--- src/Component/Nothing/Nothing.jsx
+++ src/Component/Nothing/Nothing.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Nothing() {
   return (
-    <div className="d-flex justify-content-center align-items-center">
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "300px" }}
+    >
       <h1 className="text-center ">404! Page Not Found</h1>
     </div>
   );
